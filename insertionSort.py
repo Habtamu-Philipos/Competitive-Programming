edb2ffa3591272b1af070b2cf6701530ef870a18
Habtamu-Philipos <109744538+Habtamu-Philipos@users.noreply.github.com>
2023-01-16 06:10:21 -0800

    Create insertionSort.py

---
+++ insertionSort.py
@@ -0,0 +1,29 @@
+import math
+import os
+import random
+import re
+import sys
+
+
+def insertionSort1(n, arr):
+   
+    for step in range(1,n):
+        key = arr[step]
+        j = step - 1
+        while j >= 0 and key < arr[j]:
+            arr[j+1] = arr[j]
+            j -= 1
+            print(*arr)
+            
+            
+        arr[j+1] = key
+    print(*arr)
+        
+        
+
+if __name__ == '__main__':
+    n = int(input().strip())
+
+    arr = list(map(int, input().rstrip().split()))
+
+    insertionSort1(n, arr)
