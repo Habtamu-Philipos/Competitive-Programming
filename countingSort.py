1d25a008987766173d3af6491b2ad5f268c646a0
Habtamu-Philipos <109744538+Habtamu-Philipos@users.noreply.github.com>
2023-01-16 23:26:28 -0800

    Create countingSort.py

---
+++ countingSort.py
@@ -0,0 +1,19 @@
+def countingSort(arr):
+    count = [ 0] * 100
+    for i in range(n):
+        count[arr[i]]+=1
+    return count
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    arr = list(map(int, input().rstrip().split()))
+
+    result = countingSort(arr)
+
+    fptr.write(' '.join(map(str, result)))
+    fptr.write('\n')
+
+    fptr.close()
