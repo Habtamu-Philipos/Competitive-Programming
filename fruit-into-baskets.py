f395d352ffe07b0076ddae9803ded67f1c5c06df
Habtamu-Philipos <109744538+Habtamu-Philipos@users.noreply.github.com>
2023-02-06 06:52:41 -0800

    Create fruit-into-baskets.py

---
+++ fruit-into-baskets.py
@@ -0,0 +1,18 @@
+class Solution:
+    def totalFruit(self, fruits: List[int]) -> int:
+        max_len =0
+        left = 0 
+        unique = 0
+        counter = Counter()
+        for right in range(len(fruits)):
+            counter[fruits[right]] +=1
+            if counter[fruits[right]] == 1:
+                unique +=1
+            while unique > 2:
+                counter[fruits[left]] -= 1
+                if counter[fruits[left]] == 0:
+                    unique -= 1
+                left +=1
+            cur_len = right - left + 1
+            max_len = max(max_len,cur_len)
+        return max_len
